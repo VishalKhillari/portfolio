9cd6b3d495a0a0651e3ee047709e64a163f12d42
Vishal Khillari <vishalkhillari32@gmail.com>
2026-03-01 16:36:55 +0530

    upadeted portfolio animation

--- scripts.js
+++ scripts.js
@@ -5,3 +5,18 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       .scrollIntoView({ behavior: 'smooth' });
   });
 });
+
+
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach((el) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    const visiblePoint = 100;
+
+    if (elementTop < windowHeight - visiblePoint) {
+      el.classList.add("active");
+    }
+  });
+});
